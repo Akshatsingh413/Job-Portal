8efbc21ec4278d3089cff44d9718b09f57b48de9
Akshatsingh413 <akshatbora2004@gmail.com>
2025-08-04 21:40:58 +0530

    first commit

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -74,7 +74,11 @@ const Navbar = () => {
                                             </Avatar>
                                             <div>
                                                 <h4 className='font-medium'>{user?.fullname}</h4>
-                                                <p className='text-sm text-muted-foreground'>{user?.profile?.bio}</p>
+                                                <p className='text-sm text-muted-foreground'>
+                                                    {user?.profile?.bio?.length > 50
+                                                        ? `${user.profile.bio.slice(0, 50)}...`
+                                                        : user?.profile?.bio}
+                                                </p>
                                             </div>
                                         </div>
                                         <div className='flex flex-col my-2 text-gray-600'>
