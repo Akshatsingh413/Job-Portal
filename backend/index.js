dbc84ec5bc2810010b1f0b961e737ebc90dbf363
Akshatsingh413 <akshatbora2004@gmail.com>
2025-08-05 17:52:36 +0530

    url Changed

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'https://job-portal-frontend-vauv.onrender.com',
+  origin: 'https://job-portal-h187.onrender.com',
   credentials: true,
 }));
 
