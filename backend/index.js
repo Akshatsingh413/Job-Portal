573690a9229365d8e848c2f1c616d7a32e15ec63
Akshatsingh413 <akshatbora2004@gmail.com>
2025-08-05 16:47:21 +0530

    first

--- backend/index.js
+++ backend/index.js
@@ -21,8 +21,6 @@ app.use(cookieParser());
 app.use(cors({
   origin: 'https://job-portal-frontend-vauv.onrender.com',
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 
